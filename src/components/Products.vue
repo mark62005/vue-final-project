4e9d6e0791e1623eb165ef1bc5e6466df08dc890
mark62005 <work.mark62005@gmail.com>
2020-12-08 23:31:25 +0000

    import jquery & popper.js & create modal

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div class="text-right mt-4">
-      <button class="btn btn-primary">建立新的產品</button>
+      <button class="btn btn-primary"
+        data-toggle="modal" data-target="#productModal"
+        @click="showModal"
+      >
+        建立新的產品
+      </button>
       <table class="table mt-4">
         <thead>
           <tr>
@@ -33,11 +38,35 @@
           </tr>
         </tbody>
       </table>
+      <!-- Modal -->
+      <div class="modal fade" id="productModal" tabindex="-1"
+        role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
   data() {
     return {
@@ -45,6 +74,9 @@ export default {
     };
   },
   methods: {
+    showModal() {
+      $('#productModal').modal('show');
+    },
   },
   async created() {
     const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
