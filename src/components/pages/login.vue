65f63f9366bb2d35ce627f12283bce8f032664a5
mark62005 <work.mark62005@gmail.com>
2020-12-08 20:14:54 +0000

    login & logout

---
+++ src/components/pages/login.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <form class="form-signin" @submit.prevent="signIn">
+      <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+      <label for="inputEmail" class="sr-only">電子信箱</label>
+      <input type="email" id="inputEmail" class="form-control"
+        v-model="user.username" placeholder="電子信箱" required autofocus>
+      <label for="inputPassword" class="sr-only">密碼</label>
+      <input type="password" id="inputPassword" class="form-control"
+        v-model="user.password" placeholder="密碼" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> 記住我
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">登入</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2020</p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      const api = `${process.env.API_PATH}/signin`;
+      this.axios.post(api, this.user).then((res) => {
+        console.log(res.data);
+        if (res.data.success) this.$router.push({ name: '/' });
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Login from '@/components/pages/login';
 
 Vue.use(Router);
 
@@ -10,6 +11,15 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '*',
+      redirect: '/',
     },
   ],
 });
